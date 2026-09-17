0a46db17fba3627d92664b5bee66136c04bd0003
erick-behling <erick_b_madeira@estudante.sesisenai.org.br>
2026-09-17 09:18:47 -0300

    Resolvido exercicio 4

---
+++ parte1-variaveis/exercicio_4.py
@@ -0,0 +1,5 @@
+celsius = float(input("Digite uma temperatura em graus Celsius: "))
+
+Fahrenheit = (celsius * 9) / 5 + 32
+
+print(f"O valor em graus Celsius convertido para Fahrenheit é: {Fahrenheit}")
