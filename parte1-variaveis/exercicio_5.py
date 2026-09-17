e464b803cb35ff9b4900af4d68834f8283ce58de
erick-behling <erick_b_madeira@estudante.sesisenai.org.br>
2026-09-17 09:32:23 -0300

    Resolvido exercicio 5

---
+++ parte1-variaveis/exercicio_5.py
@@ -0,0 +1,5 @@
+preco = float(input("Digite o preço do produto: "))
+qntd_comprada = float(input("Digite a quantidade comprada: "))
+
+valor_total = preco * qntd_comprada 
+print(f"O valor total da compra é: {valor_total:.2f}")
